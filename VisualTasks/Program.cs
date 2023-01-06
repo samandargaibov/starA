c689d2b83927796eba09a00bae3005ab38b65a71
samandargaibov <samandar.gaibov@gmail.com>
2023-01-06 20:01:52 +0400

    letter A

---
+++ VisualTasks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VisualTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Uzunlugu qeyd edin: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n*2; j++)
+                {
+                    if (i + j == n || j - i == n || (i == n/2 && j >= i && j<=i+n))
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
